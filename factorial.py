df6c5114d16caca5ad7f605b269a7fb5070cf7be
ruchitha.tudimella <ruchitha.tudimella@gmail.com>
2025-08-09 14:47:00 +0530

    Iterative version of factorial is added

--- factorial.py
+++ factorial.py
@@ -1,8 +1,8 @@
 def factorial(n):
-    if n == 0 or n == 1:
-        return 1
-    else:
-        return n * factorial(n - 1)
+    result = 1
+    for i in range(2, n + 1):
+        result *= i
+    return result
 
 # Example usage:
 print(factorial(5))  # Output: 120
