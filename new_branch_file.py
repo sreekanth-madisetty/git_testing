813125c9d7a71ba4548bf10a71471cafd2d6213b
ruchitha.tudimella <ruchitha.tudimella@gmail.com>
2025-08-09 21:38:24 +0530

    new branch file is commented

--- new_branch_file.py
+++ new_branch_file.py
@@ -1,7 +1,7 @@
 class Node:
     def __init__(self, data):
-        self.data = data
-        self.next = None
+        # self.data = data
+        # self.next = None
 
 class LinkedList:
     def __init__(self):
